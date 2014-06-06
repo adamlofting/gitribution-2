80c42c14de6b13852e5dc3909424c5f17615571c
Adam Lofting <contactme@adamlofting.com>
2014-06-06 12:18:31 +0100

    indexes

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -5,5 +5,7 @@ CREATE TABLE `activities` (
   `github_username` varchar(255) DEFAULT NULL,
   `github_public_email` varchar(255) DEFAULT NULL,
   `action_type` varchar(255) DEFAULT NULL,
-  UNIQUE KEY `happened_on` (`happened_on`,`github_organization`,`github_repository`,`action_type`)
+  UNIQUE KEY `happened_on` (`happened_on`,`github_organization`,`github_repository`,`action_type`),
+  KEY `activities_org_repo_index` (`github_organization`,`github_repository`),
+  KEY `activities_org_repo_date_idx` (`happened_on`,`github_organization`,`github_repository`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
