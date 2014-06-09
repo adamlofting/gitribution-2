2795d81003c9756d018ba7ae861e107be183d4e4
Adam Lofting <contactme@adamlofting.com>
2014-06-09 09:15:56 +0100

    query running totals

--- web.js
+++ web.js
@@ -7,36 +7,11 @@ var data = require("./lib/data");
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send("You're probably looking for /api or for more info about this app see https://github.com/adamlofting/gitribution");
+  res.send("You're probably looking for /api or for more info about this app see https://github.com/adamlofting/gitribution-2");
 });
 
-app.get('/api', function (req, res) {
-  var date = null;
-  var team = null;
-
-  if (req.query.date) {
-    date = new Date(req.query.date);
-    if (Object.prototype.toString.call(date) === "[object Date]") {
-      if (isNaN(date.getTime())) {
-        date = null; // date is not valid
-      }
-    } else {
-      date = null;
-    }
-  }
-
-  if (!date) {
-    res.end('Invalid parameter: "date". Please format as 2013-12-25.');
-    return;
-  }
-
-  team = req.query.team;
-  if (!team) {
-    res.end('Missing parameter: "team". E.g. webmaker, openbadges, opennews, appmaker, sciencelab.');
-    return;
-  }
-
-  data.getContributorCounts(date, team, function gotCounts(err, result) {
+app.get('/api/total/2014', function (req, res) {
+  data.get2014TotalActive(function gotCounts(err, result) {
     res.json(result);
   });
 });
