b626aa49dc1bd4c23d9ab15f9d8144c03ddee018
Adam Lofting <contactme@adamlofting.com>
2014-06-10 08:07:27 +0100

    savnig summary tables

--- fetch.js
+++ fetch.js
@@ -1,2 +1,36 @@
-var fetchData = require("./lib/fetch_data");
-fetchData.fetchMoreData();
+var async = require('async');
+var fetchData = require('./lib/fetch_data');
+var data = require('./lib/data');
+var util = require('./lib/util');
+
+async.waterfall([
+    function saveSummaryForAll (callback){
+      // get the total combined numbers
+      data.update2014TotalActive(null, function updatedAll (err) {
+        callback(null);
+      });
+    },
+    function saveSummaryForTeams (callback){
+      var teams = util.validTeamNames();
+
+      async.each(teams,
+        function toEach (team, callback) {
+          data.update2014TotalActive(team, function updatedAll (err) {
+            callback(null);
+          });
+        },
+        function eachDone (err) {
+          callback(null);
+        }
+      );
+    },
+    function fetchLatestGithubData (callback){
+      fetchData.fetchMoreData(function fetched (err, res) {
+        callback(null);
+      });
+    }
+], function (err, result) {
+   console.log('summaries and tables updated');
+   process.exit(0);
+});
+
--- sql/create_table.sql
+++ sql/create_table.sql
@@ -9,3 +9,13 @@ CREATE TABLE `activities` (
   KEY `activities_org_repo_index` (`github_organization`,`github_repository`),
   KEY `activities_org_repo_date_idx` (`happened_on`,`github_organization`,`github_repository`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `summary` (
+  `team` varchar(45) NOT NULL,
+  `type` varchar(45) NOT NULL,
+  `wkcommencing` date NOT NULL,
+  `totalactive` int(11) NOT NULL,
+  `new` int(11) NOT NULL,
+  UNIQUE KEY `unique_key` (`team`,`type`,`wkcommencing`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
