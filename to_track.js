74a5dc07c25ec9da542ece50214ff2400d66b8d4
Adam Lofting <contactme@adamlofting.com>
2014-11-25 10:01:38 +0000

    add mozillafordevelopment org

--- to_track.js
+++ to_track.js
@@ -1,2 +1,2 @@
-exports.orgs = ['mozilla-appmaker', 'mozilla', 'mozilla-b2g', 'mozilla-services'];
+exports.orgs = ['mozilla-appmaker', 'mozilla', 'mozilla-b2g', 'mozilla-services', 'mozillafordevelopment'];
 //exports.orgs = ['mozilla-b2g'];
